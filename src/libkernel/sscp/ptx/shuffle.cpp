bb83e86f9b57884f10279d7d9966af4c12dd838b
sbalint98 <sbsbalint14@gmail.com>
2024-11-11 14:00:40 +0100

    We don't need to have to calculate the mask for the cuda functiuons
    Also disable tests that are not yet functional for CUDA

--- src/libkernel/sscp/ptx/shuffle.cpp
+++ src/libkernel/sscp/ptx/shuffle.cpp
@@ -29,11 +29,12 @@
  #include "hipSYCL/sycl/libkernel/sscp/builtins/shuffle.hpp"
  #include "hipSYCL/sycl/libkernel/sscp/builtins/subgroup.hpp"
 
-__acpp_uint32 get_active_mask(){
-    //__acpp_int64 mask = __nvvm_activemask();
-    __acpp_uint32 subgroup_size = __acpp_sscp_get_subgroup_size();
-    return (1 << subgroup_size)-1;
-}
+// __acpp_uint32 get_active_mask(){
+//     //__acpp_int64 mask = __nvvm_activemask();
+//     __acpp_uint32 subgroup_size = __acpp_sscp_get_subgroup_size();
+//     return (1 << subgroup_size)-1;
+// }
+unsigned int FULL_MASk = 0xffffffff;
 
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
 __acpp_int8 __acpp_sscp_sub_group_shl_i8(__acpp_int8 value,
@@ -50,8 +51,8 @@ __acpp_int16 __acpp_sscp_sub_group_shl_i16(__acpp_int16 value,
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
 __acpp_int32 __acpp_sscp_sub_group_shl_i32(__acpp_int32 value,
                                                   __acpp_uint32 delta){
-    __acpp_uint32 mask = get_active_mask();
-    return __nvvm_shfl_sync_down_i32(mask, value, delta, 0x1f);
+    // __acpp_uint32 mask = get_active_mask();
+    return __nvvm_shfl_sync_down_i32(FULL_MASk, value, delta, 0x1f);
                                                   }
 
 
@@ -98,8 +99,8 @@ __acpp_int16 __acpp_sscp_sub_group_shr_i16(__acpp_int16 value,
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
 __acpp_int32 __acpp_sscp_sub_group_shr_i32(__acpp_int32 value,
                                                   __acpp_uint32 delta){
-    __acpp_uint32 mask = get_active_mask();
-    return __nvvm_shfl_sync_up_i32(mask,value, delta, 0);
+    // __acpp_uint32 mask = get_active_mask();
+    return __nvvm_shfl_sync_up_i32(FULL_MASk,value, delta, 0);
                                                   }
 
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
@@ -130,8 +131,8 @@ __acpp_int16 __acpp_sscp_sub_group_permute_i16(__acpp_int16 value,
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
 __acpp_int32 __acpp_sscp_sub_group_permute_i32(__acpp_int32 value,
                                                      __acpp_int32 mask){
-    __acpp_uint32 active_thread_mask = get_active_mask();
-    return __nvvm_shfl_sync_bfly_i32(active_thread_mask,value, mask, 0x1f);
+    // __acpp_uint32 active_thread_mask = get_active_mask();
+    return __nvvm_shfl_sync_bfly_i32(FULL_MASk,value, mask, 0x1f);
                                                   }
 
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
@@ -165,8 +166,8 @@ __acpp_int32 __acpp_sscp_sub_group_select_i32(__acpp_int32 value,
     // int max_subgroup_size = __acpp_sscp_get_subgroup_max_size();
     // int index = id%max_subgroup_size;
     // return __builtin_amdgcn_ds_bpermute(index<<2, value);
-    __acpp_uint32 mask = get_active_mask();
-    return __nvvm_shfl_sync_idx_i32(mask, value, id, 0);
+    // __acpp_uint32 mask = get_active_mask();
+    return __nvvm_shfl_sync_idx_i32(FULL_MASk, value, id, 0);
                                                   }
 
 HIPSYCL_SSCP_CONVERGENT_BUILTIN
