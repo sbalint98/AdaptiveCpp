1208f8f5753ac13acf33c45cfd0530bfb4116984
sbalint98 <sbsbalint14@gmail.com>
2024-11-12 22:26:15 +0100

    Add the host reduction implementation

---
+++ src/libkernel/sscp/host/reduce.cpp
@@ -0,0 +1,85 @@
+/*
+ * This file is part of hipSYCL, a SYCL implementation based on CUDA/HIP
+ *
+ * Copyright (c) 2024 Aksel Alpay
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "hipSYCL/sycl/libkernel/sscp/builtins/reduction.hpp"
+
+
+
+#define SUBGROUP_FLOAT_REDUCTION(type) \
+HIPSYCL_SSCP_CONVERGENT_BUILTIN \
+__acpp_##type __acpp_sscp_sub_group_reduce_##type(__acpp_sscp_algorithm_op op, __acpp_##type x){ \
+    switch(op) \
+    { \
+        case __acpp_sscp_algorithm_op::plus: \
+            return __acpp_reduce_over_group_##type(x, plus{}); \
+        case __acpp_sscp_algorithm_op::multiply: \
+            return __acpp_reduce_over_group_##type(x, multiply{}); \
+        case __acpp_sscp_algorithm_op::min: \
+            return __acpp_reduce_over_group_##type(x, min{}); \
+        case __acpp_sscp_algorithm_op::max: \
+            return __acpp_reduce_over_group_##type(x, max{}); \
+    } \
+} \
+
+SUBGROUP_FLOAT_REDUCTION(f32)
+SUBGROUP_FLOAT_REDUCTION(f64)
+
+#define SUBGROUP_INT_REDUCTION(type) \
+HIPSYCL_SSCP_CONVERGENT_BUILTIN \
+__acpp_##type __acpp_sscp_sub_group_reduce_##type(__acpp_sscp_algorithm_op op, __acpp_##type x){ \
+    switch(op) \
+    { \
+        case __acpp_sscp_algorithm_op::plus: \
+            return __acpp_reduce_over_group_##type(x, plus{}); \
+        case __acpp_sscp_algorithm_op::multiply: \
+            return __acpp_reduce_over_group_##type(x, multiply{}); \
+        case __acpp_sscp_algorithm_op::min: \
+            return __acpp_reduce_over_group_##type(x, min{}); \
+        case __acpp_sscp_algorithm_op::max: \
+            return __acpp_reduce_over_group_##type(x, max{}); \
+        case __acpp_sscp_algorithm_op::bit_and: \
+            return __acpp_reduce_over_group_##type(x, bit_and{}); \
+        case __acpp_sscp_algorithm_op::bit_or: \
+            return __acpp_reduce_over_group_##type(x, bit_or{}); \
+        case __acpp_sscp_algorithm_op::bit_xor: \
+            return __acpp_reduce_over_group_##type(x, bit_xor{}); \
+        case __acpp_sscp_algorithm_op::logical_and: \
+            return __acpp_reduce_over_group_##type(x, logical_and{}); \
+        case __acpp_sscp_algorithm_op::logical_or: \
+            return __acpp_reduce_over_group_##type(x, logical_or{}); \
+    } \
+} \
+
+SUBGROUP_INT_REDUCTION(int8)
+SUBGROUP_INT_REDUCTION(int16)
+SUBGROUP_INT_REDUCTION(int32)
+SUBGROUP_INT_REDUCTION(int64)
+
+SUBGROUP_INT_REDUCTION(uint8)
+SUBGROUP_INT_REDUCTION(uint16)
+SUBGROUP_INT_REDUCTION(uint32)
+SUBGROUP_INT_REDUCTION(uint64)
