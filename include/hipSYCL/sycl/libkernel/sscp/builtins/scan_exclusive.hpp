e5a8cc4c2c1849cd68f28080b481c72cfe8721f4
sbalint98 <sbsbalint14@gmail.com>
2024-12-02 10:45:00 +0100

    Use the old scanning algo for now

--- include/hipSYCL/sycl/libkernel/sscp/builtins/scan_exclusive.hpp
+++ include/hipSYCL/sycl/libkernel/sscp/builtins/scan_exclusive.hpp
@@ -87,65 +87,57 @@ OutType __acpp_group_exclusive_scan_impl(OutType x,BinaryOperation op, OutType i
 
   // OutType sg_scan_result = __acpp_subgroup_exclusive_scan_impl(x, op, sg_size);
   OutType sg_scan_result = __acpp_subgroup_exclusive_scan_impl(x, op, init);
-  // return sg_scan_result;
-  if(last_item_in_sg){
-    shrd_mem[subgroup_id] = op(sg_scan_result,x);
-  }
-  __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
-  if(subgroup_id == 0){
-    shrd_mem[wg_lid] =  __acpp_subgroup_exclusive_scan_impl(shrd_mem[wg_lid], op, init);
-  }
-  __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
-  return subgroup_id > 0 ? op(shrd_mem[subgroup_id], sg_scan_result) : sg_scan_result;
-  
-  //auto result = op(sg_scan_result, shrd_mem[subgroup_id]);
-  //return sg_scan_result;
-  // return shrd_mem[subgroup_id];
-  //return op(sg_scan_result, shrd_mem[subgroup_id]);
-  // auto result = op(shrd_mem[subgroup_id], sg_scan_result);
-  // return result;
-
-
-  // for(int i = 0; i < (num_subgroups-1+shmem_array_length)/shmem_array_length; i++){
-  //   __acpp_uint32 first_active_thread = i*num_subgroups*max_sg_size;
-  //   __acpp_uint32 last_active_thread  = (i+1)*num_subgroups*max_sg_size;
-  //   last_active_thread  =  last_active_thread > wg_size ? wg_size : last_active_thread;
-  //   __acpp_uint32 relative_thread_id = wg_lid - first_active_thread;
-  //   if(subgroup_id/shmem_array_length == i){
-  //     if(last_item_in_sg){
-  //     // return 1212;
-
-  //       // return sg_scan_result;
-  //       shrd_mem[subgroup_id%shmem_array_length] = sg_scan_result + x;
-  //     }
-  //   }
-  //   __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
-  //   // First shmem_array_length number of threads exclusive scan in shared memory
-  //     auto local_x = shrd_mem[relative_thread_id];
-  //     for (__acpp_int32 j = 1; j < shmem_array_length; j *= 2) {  
-  //       __acpp_int32 next_id = relative_thread_id -j; 
-  //       if (next_id >= 0 && j <= relative_thread_id){
-  //         if(relative_thread_id < shmem_array_length){
-  //           auto other_x= shrd_mem[next_id];
-  //           local_x = op(local_x, other_x);
-  //           shrd_mem[relative_thread_id] = local_x;
-  //         }
-  //       }
-  //       __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
-  //     }
-  //     __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
-
-  //   if(subgroup_id > 0){
-  //     auto current_segment_update = shrd_mem[(subgroup_id%shmem_array_length)-1];
-  //     sg_scan_result = op(current_segment_update, sg_scan_result);
-  //   }
-  //   if(i>0){
-  //     sg_scan_result = op(shrd_mem[shmem_array_length], sg_scan_result);
-  //   }
-  //   __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
-  //   shrd_mem[shmem_array_length] = sg_scan_result;
+  //COde for CUDA and HIP versions
+  // if(last_item_in_sg){
+  //   shrd_mem[subgroup_id] = op(sg_scan_result,x);
   // }
-  // return sg_scan_result;
+  // __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
+  // if(subgroup_id == 0){
+  //   shrd_mem[wg_lid] =  __acpp_subgroup_exclusive_scan_impl(shrd_mem[wg_lid], op, init);
+  // }
+  // __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
+  // return subgroup_id > 0 ? op(shrd_mem[subgroup_id], sg_scan_result) : sg_scan_result;
+
+  for(int i = 0; i < (num_subgroups-1+shmem_array_length)/shmem_array_length; i++){
+    __acpp_uint32 first_active_thread = i*num_subgroups*max_sg_size;
+    __acpp_uint32 last_active_thread  = (i+1)*num_subgroups*max_sg_size;
+    last_active_thread  =  last_active_thread > wg_size ? wg_size : last_active_thread;
+    __acpp_uint32 relative_thread_id = wg_lid - first_active_thread;
+    if(subgroup_id/shmem_array_length == i){
+      if(last_item_in_sg){
+      // return 1212;
+
+        // return sg_scan_result;
+        shrd_mem[subgroup_id%shmem_array_length] = op(sg_scan_result, x);
+      }
+    }
+    __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
+    // First shmem_array_length number of threads exclusive scan in shared memory
+      auto local_x = shrd_mem[relative_thread_id];
+      for (__acpp_int32 j = 1; j < shmem_array_length; j *= 2) {  
+        __acpp_int32 next_id = relative_thread_id -j; 
+        if (next_id >= 0 && j <= relative_thread_id){
+          if(relative_thread_id < shmem_array_length){
+            auto other_x= shrd_mem[next_id];
+            local_x = op(local_x, other_x);
+            shrd_mem[relative_thread_id] = local_x;
+          }
+        }
+        __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
+      }
+      __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
+
+    if(subgroup_id > 0){
+      auto current_segment_update = shrd_mem[(subgroup_id%shmem_array_length)-1];
+      sg_scan_result = op(current_segment_update, sg_scan_result);
+    }
+    if(i>0){
+      sg_scan_result = op(shrd_mem[shmem_array_length], sg_scan_result);
+    }
+    __acpp_sscp_work_group_barrier(__acpp_sscp_memory_scope::work_group, __acpp_sscp_memory_order::relaxed);
+    shrd_mem[shmem_array_length] = sg_scan_result;
+  }
+  return sg_scan_result;
 }
 
 #endif
